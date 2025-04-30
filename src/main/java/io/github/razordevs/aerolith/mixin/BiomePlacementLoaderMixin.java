d169fb89f253584b220a6a8022d292c1ccfd9293
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-04-30 14:26:00 +0200

    work: SurfaceRules injection works, biomes do not

--- src/main/java/io/github/razordevs/aerolith/mixin/BiomePlacementLoaderMixin.java
+++ src/main/java/io/github/razordevs/aerolith/mixin/BiomePlacementLoaderMixin.java
@@ -1,18 +1,24 @@
 package io.github.razordevs.aerolith.mixin;
 
 import com.terraformersmc.biolith.impl.data.BiomePlacementLoader;
+import com.terraformersmc.biolith.impl.data.BiomePlacementMarshaller;
 import io.github.razordevs.aerolith.biome.BiomePlacementHelper;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(BiomePlacementLoader.class)
+import java.util.List;
+
+@Mixin(value = BiomePlacementLoader.class, remap = false)
 public class BiomePlacementLoaderMixin {
 
-    @Inject(method = "apply*", at = @At(value = "INVOKE", target = "Lcom/terraformersmc/biolith/impl/biome/EndBiomePlacement;clearFromData()V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void apply(CallbackInfo ci) {
+    @Inject(method = "apply(Ljava/util/List;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
+            at = @At(value = "INVOKE", target = "Lcom/terraformersmc/biolith/impl/biome/EndBiomePlacement;clearFromData()V", shift = At.Shift.AFTER))
+
+    public void aerolith$onApply(List<BiomePlacementMarshaller> marshallers, ResourceManager manager, ProfilerFiller profiler, CallbackInfo ci) {
         BiomePlacementHelper.AETHER.clearFromData();
     }
 }
---
+++ src/main/java/io/github/razordevs/aerolith/mixin/MultiNoiseBiomeSourceMixin.java
@@ -0,0 +1,95 @@
+package io.github.razordevs.aerolith.mixin;
+
+import com.aetherteam.aether.data.resources.registries.AetherDimensions;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.datafixers.util.Either;
+import com.mojang.datafixers.util.Pair;
+import com.terraformersmc.biolith.api.biome.BiolithFittestNodes;
+import com.terraformersmc.biolith.impl.biome.InterfaceBiomeSource;
+import com.terraformersmc.biolith.impl.compat.VanillaCompat;
+import io.github.razordevs.aerolith.biome.BiomePlacementHelper;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.*;
+import net.minecraft.world.level.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+@Mixin(value = MultiNoiseBiomeSource.class, priority = 800)
+public abstract class MultiNoiseBiomeSourceMixin extends BiomeSource implements InterfaceBiomeSource  {
+    private ResourceKey<DimensionType> aerolith$dimensionType = InterfaceBiomeSource.DIMENSION_TYPE_UNDEFINED;
+
+    @Shadow
+    protected abstract Climate.ParameterList<Holder<Biome>> parameters();
+
+    private Climate.ParameterList<Holder<Biome>> aerolith$biomeParameterList;
+
+    // Inject noise points the first time somebody requests them.
+    @WrapOperation(
+            method = "parameters",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/datafixers/util/Either;map(Ljava/util/function/Function;Ljava/util/function/Function;)Ljava/lang/Object;")
+    )
+    @SuppressWarnings("unused")
+    private Object aerolith$injectParameterList(Either<Climate.ParameterList<Holder<Biome>>, Holder<MultiNoiseBiomeSourceParameterList>> instance,
+                                                Function<Climate.ParameterList<Holder<Biome>>, Climate.ParameterList<Holder<Biome>>> leftMap,
+                                                Function<Holder<MultiNoiseBiomeSourceParameterList>, Climate.ParameterList<Holder<Biome>>> rightMap, Operation<Object> original) {
+        synchronized (this) {
+            // Only compute this once, since our version is more expensive than Mojang's.
+            if (aerolith$biomeParameterList == null) {
+                // Mojang does the exact same cast on the return of this operation.
+                //noinspection unchecked
+                Climate.ParameterList<Holder<Biome>> originalParameterList =
+                        (Climate.ParameterList<Holder<Biome>>) original.call(instance, leftMap, rightMap);
+
+                if (this.aerolith$getDimensionType().location().equals(AetherDimensions.AETHER_DIMENSION_TYPE.location())) {
+                    List<Pair<Climate.ParameterPoint, Holder<Biome>>> parameterList = new ArrayList<>(256);
+
+                    // Remove any biomes matching removals
+                    originalParameterList.values().stream()
+                            .filter(BiomePlacementHelper.AETHER::removalFilter)
+                            .forEach(parameterList::add);
+
+                    // Add all biomes from additions, replacements, and sub-biome requests
+                    BiomePlacementHelper.AETHER.writeBiomeEntries(parameterList::add);
+
+                    aerolith$biomeParameterList = new Climate.ParameterList<>(parameterList);
+                } else {
+                    aerolith$biomeParameterList = originalParameterList;
+                }
+            }
+        } // synchronized (this)
+
+        return aerolith$biomeParameterList;
+    }
+
+    // We calculate the vanilla/datapack biome, then we apply any overlays.
+    @Inject(method = "getNoiseBiome(IIILnet/minecraft/world/level/biome/Climate$Sampler;)Lnet/minecraft/core/Holder;", at = @At("HEAD"), cancellable = true)
+    private void biolith$getBiome(int x, int y, int z, Climate.Sampler noise, CallbackInfoReturnable<Holder<Biome>> cir) {
+        Climate.TargetPoint noisePoint = noise.sample(x, y, z);
+        BiolithFittestNodes<Holder<Biome>> fittestNodes = null;
+
+        // Find the biome via Vanilla (including datapacks) if none was provided by TerraBlender.
+        if (fittestNodes == null) {
+            fittestNodes = VanillaCompat.getBiome(noisePoint, parameters());
+        }
+
+        // Apply biome overlays.
+        if (this.aerolith$getDimensionType().registry().equals(AetherDimensions.AETHER_DIMENSION_TYPE.location())) {
+            cir.setReturnValue(BiomePlacementHelper.AETHER.getReplacement(x, y, z, noisePoint, fittestNodes));
+        } else {
+            cir.setReturnValue(fittestNodes.ultimate().value);
+        }
+    }
+
+    public ResourceKey<DimensionType> aerolith$getDimensionType() {
+        return aerolith$dimensionType;
+    }
+}
