f94000290073f535e2779679c9b1c5a8a538212a
darksonic300 <fabiross096@gmail.com>
2025-09-21 02:19:25 +0200

    fix: added Config registration

--- src/main/java/io/github/razordevs/aerolith/Aerolith.java
+++ src/main/java/io/github/razordevs/aerolith/Aerolith.java
@@ -8,7 +8,9 @@ import io.github.razordevs.aerolith.biome.AetherBiomePlacement;
 import io.github.razordevs.aerolith.biome.BiomePlacementHelper;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.server.ServerStoppedEvent;
 import org.slf4j.Logger;
@@ -16,20 +18,15 @@ import org.slf4j.Logger;
 @Mod("aerolith")
 public class Aerolith {
     public static final Logger LOGGER = LogUtils.getLogger();
-//    private static final AerolithConfigManager CONFIG_MANAGER = new AerolithConfigManager();
 
     public static final String MODID = "aerolith";
     public static final String MOD_VERSION = "0.0.1";
 
-    public Aerolith() {
+    public Aerolith(ModContainer mod) {
         LOGGER.info("Aerolith Initialized");
 
         NeoForge.EVENT_BUS.addListener((ServerStoppedEvent event) -> AetherBiomeCoordinator.handleServerStopped());
 
-//        CONFIG_MANAGER.getAerolithConfig();
+        mod.registerConfig(ModConfig.Type.COMMON, AerolithConfig.COMMON_SPEC);
     }
-
-//    public static AerolithConfigManager getConfigManager() {
-//        return CONFIG_MANAGER;
-//    }
 }
