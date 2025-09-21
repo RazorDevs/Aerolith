fee27723b8ca0b29d9047949efd47dda7e8bd626
darksonic300 <fabiross096@gmail.com>
2025-09-21 11:55:49 +0200

    work: added TODOs

--- src/main/java/io/github/razordevs/aerolith/Aerolith.java
+++ src/main/java/io/github/razordevs/aerolith/Aerolith.java
@@ -1,13 +1,7 @@
 package io.github.razordevs.aerolith;
 
-import com.aetherteam.aether.data.resources.registries.AetherBiomes;
 import com.mojang.logging.LogUtils;
-import com.terraformersmc.biolith.api.biome.BiomePlacement;
 import io.github.razordevs.aerolith.biome.AetherBiomeCoordinator;
-import io.github.razordevs.aerolith.biome.AetherBiomePlacement;
-import io.github.razordevs.aerolith.biome.BiomePlacementHelper;
-import net.minecraft.world.level.biome.Biomes;
-import net.minecraft.world.level.biome.Climate;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
@@ -18,6 +12,10 @@ import org.slf4j.Logger;
 @Mod("aerolith")
 public class Aerolith {
     public static final Logger LOGGER = LogUtils.getLogger();
+
+    //TODO: Figure out removal of Forgified Api requirement
+    //TODO: Handle a weight parameter for each entry that uses Aerolith
+
 
     public static final String MODID = "aerolith";
     public static final String MOD_VERSION = "0.0.1";
